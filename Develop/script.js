edd34fdf807422011ed29c6c09381ff608f725c5
James Weah <jweah@aggies.ncat.edu>
2023-11-13 14:40:36 -0500

    save to local storage and hourColor bug fixed

--- Develop/script.js
+++ Develop/script.js
@@ -12,29 +12,43 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   // End of Number 22
+
+  $('.saveBtn').on('click', function () {
+    // Get the id of the parent time-block
+    let timeBlockId = $(this).closest('.time-block').attr('id');
+
+    // Get the user input from the textarea within the clicked time-block
+    let userInput = $(this).siblings('.description').val();
+
+    // Save the user input in local storage with the time-block id as the key
+    localStorage.setItem(timeBlockId, userInput);
+  });
   let hour = dayjs().format('H');
 
   
-  
+
   const formattedDate = dayjs().format('dddd MMMM DD');
 
   console.log(formattedDate);
   
   $('#currentDay').text(formattedDate);
-  function hourColor() {
-    $('.time-block').each(function() {
-      let eachHour = this.id;
-      
 
-      if(eachHour == hour) {
+
+  function hourColor() {
+    $('.time-block').each(function () {
+      let eachHour = parseInt(this.id.split('-')[1]); // Extract the hour and convert to integer
+      let currentTime = parseInt(hour);
+
+      if (eachHour === currentTime) {
         $(this).removeClass('past future').addClass('present');
-      } else if (eachHour > hour) {
+      } else if (eachHour > currentTime) {
         $(this).removeClass('past present').addClass('future');
-      } else if (eachHour < hour) {
+      } else if (eachHour < currentTime) {
         $(this).removeClass('present future').addClass('past');
       }
-    })
+    });
   }
+
 
 
   $(document).ready(function() {
@@ -52,11 +66,29 @@ $(function () {
   // current hour in 24-hour time?
   //
   //
+  //Number 24
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  //Ender of 24
   //
-  //
+  
+    $(document).ready(function () {
+      // Number 24
+      // Retrieve and set user input from local storage
+      $('.time-block').each(function () {
+        let timeBlockId = $(this).attr('id');
+        let storedInput = localStorage.getItem(timeBlockId);
+
+        if (storedInput) {
+          $(this).find('.description').val(storedInput);
+        }
+      });
+      // End of Number 24
+
+      hourColor();
+    });
+
   // Number 23
   // TODO: Add code to display the current date in the header of the page.
   // Ender of 23
